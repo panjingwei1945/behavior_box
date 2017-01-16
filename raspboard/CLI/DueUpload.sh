69570eecacf84a49cb9f8e4e847a57fb9c968d84
panjingwei1945 <panjingwei1945@hotmail.com>
2017-01-16 19:32:17 +0800

    add DueCompileUpload.sh

--- raspboard/CLI/DueUpload.sh
+++ raspboard/CLI/DueUpload.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-FIRMWARE=$1
+
 BOSSAC="/home/pi/.arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac"
+
+if[ -n ${BINFILE} ]
+then
+  FIRMWARE=${BINFILE}
+else
+  FIRMWARE=$1
+fi
 
 #upload
 stty -F /dev/ttyAMA0 speed 1200 cs8;${BOSSAC} -i -d --port=ttyAMA0 -U false -e -w -v -b ${FIRMWARE} -R
