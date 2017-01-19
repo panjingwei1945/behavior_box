f839fd6383a92ecc37ccc14a1ebbb6e00280f829
panjingwei1945 <panjingwei1945@hotmail.com>
2017-01-19 17:36:46 +0800

    edit readme.md again.

--- raspboard/ATX/reset_service.sh
+++ raspboard/ATX/reset_service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 filename="syson_pin.sh"
 echo '#!/bin/bash
 #when system reset, set pin17 to high.Then hold on 500ms.
@@ -6,6 +7,6 @@ syson=17
 echo "$syson" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$SHUTDOWN/direction
 echo "1" > /sys/class/gpio/gpio$SHUTDOWN/value' > /tmp/${filename}
-sudo chmod 755 /tmp/${filename}
-sudo mv /tmp/${filename} /etc/init.d/${filename}
-sudo ln -s /etc/init.d/${filename} /etc/rc6.d/K01${filename}
+chmod 755 /tmp/${filename}
+mv /tmp/${filename} /etc/init.d/${filename}
+ln -s /etc/init.d/${filename} /etc/rc6.d/K01${filename}
--- raspboard/ATX/sys_on_service.sh
+++ raspboard/ATX/sys_on_service.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo '#!/bin/bash
 
 #when system turn on, set pin26 to high.
@@ -5,5 +7,5 @@ syson=26
 echo "$syson" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$SHUTDOWN/direction
 echo "1" > /sys/class/gpio/gpio$SHUTDOWN/value' > /etc/syson_pin.sh
-sudo chmod 755 /etc/syson_pin.sh
-sudo sed -i '$ i /etc/syson_pin.sh &' /etc/rc.local
+chmod 755 /etc/syson_pin.sh
+sed -i '$ i /etc/syson_pin.sh &' /etc/rc.local
